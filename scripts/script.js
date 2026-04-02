68f6ec855d7a4040c7570eb7ba734755681ec929
Alessandro Tiberia <tiberia7030@gmail.com>
2026-04-02 16:43:32 +0200

    esercizio base completato (sostituita img con click)

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,15 @@ Al click del bottone, la lampadina dovrà accendersi
 (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
  */
 
-const elemento = document.querySelector("#mio-id-element");
+const elemento = document.querySelector("#mio-id-element"); //variabile con id element
+
+const bottone = document.querySelector("#mio-bottone"); //variabile con id bottone
+
+function luce_accesa() { 
+   elemento.src= "./img/yellow_lamp.png"; // prendo src e cambio da spento ad acceso 
+   alt="lampadina accesa"; // modifico anche alt di img
+}
+
+bottone.addEventListener ("click", luce_accesa); //rimani in "ascolto" finchè non cliccano sul bottone 
+// niente parentesi graffa ricordare
+
