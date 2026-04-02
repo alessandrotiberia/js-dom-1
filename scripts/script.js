eab4fbf22dfdea22a06873e7f79e5d2200644cb2
Alessandro Tiberia <tiberia7030@gmail.com>
2026-04-02 16:50:49 +0200

    corretto codice dentro function

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+'use strict'
+
 /*Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato
  e accanto un bottone con la scritta “Accendi”.
 Al click del bottone, la lampadina dovrà accendersi 
@@ -8,11 +10,12 @@ const elemento = document.querySelector("#mio-id-element"); //variabile con id e
 
 const bottone = document.querySelector("#mio-bottone"); //variabile con id bottone
 
-function luce_accesa() { 
-   elemento.src= "./img/yellow_lamp.png"; // prendo src e cambio da spento ad acceso 
-   alt="lampadina accesa"; // modifico anche alt di img
+function luce_accesa() {
+    elemento.src = "./img/yellow_lamp.png"; // prendo src e cambio da spento ad acceso 
+    elemento.alt = "lampadina accesa"; // modifico anche alt di img
 }
 
-bottone.addEventListener ("click", luce_accesa); //rimani in "ascolto" finchè non cliccano sul bottone 
+bottone.addEventListener("click", luce_accesa); //rimani in "ascolto" finchè non cliccano sul bottone 
 // niente parentesi graffa ricordare
+
 
